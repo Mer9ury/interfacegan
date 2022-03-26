90aefc73754fa25e487a9b3789777cbc8d678a17
mer9ury <bbarroo7425@gmail.com>
2022-03-26 15:40:59 +0000

    implement shell script download seperator

---
+++ auxiliary/download_seperator.sh
@@ -0,0 +1,6 @@
+wget https://nvlabs-fi-cdn.nvidia.com/stylegan/networks/metrics/celebahq-classifier-00-male.pkl
+wget https://nvlabs-fi-cdn.nvidia.com/stylegan/networks/metrics/celebahq-classifier-04-young.pkl
+wget https://nvlabs-fi-cdn.nvidia.com/stylegan/networks/metrics/celebahq-classifier-19-eyeglasses.pkl
+mkdir pretrain
+mv *.pkl pretrain
+
